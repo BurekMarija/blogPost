6a5429ee8b190bc7c924082482db03780d7b024d
BurekMarija <marija.balun@gmail.com>
2023-01-25 23:03:46 +0100

    error nakon apk

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
